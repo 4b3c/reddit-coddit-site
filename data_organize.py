c3cf9bb332190507b2d39a9a801e723835a1ac3b
Abram <58718191+heyuhowudoin@users.noreply.github.com>
2021-09-15 20:47:14 -0700

    changed to a hosted file

--- data_organize.py
+++ data_organize.py
@@ -5,53 +5,6 @@ url = "https://www.reddit.com/r/teenagersbutpog/comments/pjg3lj/coddit_20_but_on
 post = reddit.submission(url = url)
 data = {}
 
-list_of_users = []
-by_comment = {}
-by_post = {}
-by_contributions = {}
-
-def get_by_posts(contribution_data):
-	return contribution_data[1]
-
-def get_by_comments(contribution_data):
-	return contribution_data[2]
-
-def get_by_contributions(contribution_data):
-	return contribution_data[1] + contribution_data[2]
-
-
-with open("data.json") as data_file:
-	data = json.load(data_file)
-
-	for user in data:
-		list_of_users.append([user, data.get(user)[0], data.get(user)[1]])
-
-	list_of_users.sort(key = get_by_comments, reverse = True)
-
-	for i in list_of_users:
-		by_comment[i[0]] = [i[1], i[2]]
-
-	with open("data_by_comment.json", "w") as data_file:
-		json.dump(by_comment, data_file, indent = 3)
-
-
-	list_of_users.sort(key = get_by_posts, reverse = True)
-
-	for i in list_of_users:
-		by_post[i[0]] = [i[1], i[2]]
-
-	with open("data_by_post.json", "w") as data_file:
-		json.dump(by_post, data_file, indent = 3)
-
-
-	list_of_users.sort(key = get_by_contributions, reverse = True)
-
-	for i in list_of_users:
-		by_contributions[i[0]] = [i[1], i[2]]
-
-	with open("data_by_contribution.json", "w") as data_file:
-		json.dump(by_contributions, data_file, indent = 3)
-		
 with open("data.json") as data_file:
 	data = json.load(data_file)
 	
