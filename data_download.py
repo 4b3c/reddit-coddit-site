ab6321c658e71a1cd51deb2d8743635f4df877bf
Abram <58718191+heyuhowudoin@users.noreply.github.com>
2021-09-17 16:52:29 -0700

    auto push

--- data_download.py
+++ data_download.py
@@ -4,8 +4,8 @@ reddit = login_info.reddit
 submission_stream = reddit.subreddit("teenagersbutpog").stream.submissions(pause_after = -1, skip_existing = True)
 comment_stream = reddit.subreddit("teenagersbutpog").stream.comments(pause_after = -1, skip_existing = True)
 data = {}
-changed_list = []
-old_times = []
+# changed_list = []
+# old_times = []
 
 def store(author, created_time, contribution_type):
 	with open('data.json') as data_file:
@@ -13,43 +13,40 @@ def store(author, created_time, contribution_type):
 
 		if (author in data):
 			data[author][contribution_type] += 1
-			data[author][contribution_type + 2].append(created_time)
-			changed_list.append(author)
+			# data[author][contribution_type + 2].append(created_time)
+			# changed_list.append(author)
 
 		elif (author not in data):
-			data[author] = [0, 0, [], []]
+			data[author] = [0, 0]
 			data[author][contribution_type] += 1
-			data[author][contribution_type + 2].append(created_time)
-			changed_list.append(author)
+			# data[author][contribution_type + 2].append(created_time)
+			# changed_list.append(author)
 
 	with open("data.json", "w") as data_file:
 		json.dump(data, data_file, indent = 3)
 
 while True:
-	try:
-		for post in submission_stream:
-			if post is None:
-				break
-			store(str(post.author), post.created_utc, 0)
+	for post in submission_stream:
+		if post is None:
+			break
+		store(str(post.author), post.created_utc, 0)
 
-		for comment in comment_stream:
-			if comment is None:
-				break
-			store(str(comment.author), comment.created_utc, 1)
+	for comment in comment_stream:
+		if comment is None:
+			break
+		store(str(comment.author), comment.created_utc, 1)
 
-		with open("data.json") as data_file:
-			data = json.load(data_file)
-			
-			for user in data:
-				data.get(user)[2] = 0
-				for time in data.get(user)[3]:
-					if (time > time_.time() - 604800):
-						data.get(user)[2] = data.get(user)[2] + 1
-					else:
-						old_times.append(time)
+	# with open("data.json") as data_file:
+	# 	data = json.load(data_file)
+		
+		# for user in data:
+		# 	data.get(user)[2] = 0
+		# 	for time in data.get(user)[3]:
+		# 		if (time > time_.time() - 604800):
+		# 			data.get(user)[2] = data.get(user)[2] + 1
+		# 		else:
+		# 			old_times.append(time)
 
-				for i in old_times:
-					data.get(user)[3].remove(i)
-				old_times = []
-	except:
-		print("ERROR \r\r\r\r\r\r\r")
+		# 	for i in old_times:
+		# 		data.get(user)[3].remove(i)
+		# 	old_times = []
